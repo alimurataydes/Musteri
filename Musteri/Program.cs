923ddc296f97395f635b36b76eb0e701c123d9bf
ali murat <ali murat@DESKTOP-PFPSATV>
2021-02-13 10:24:13 +0300

    Add project files.

---
+++ Musteri/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Musteri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
